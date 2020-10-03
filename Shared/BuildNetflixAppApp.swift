c7aa8df40e2c54de1957956e1723dd3d48ff4700
Temi <ytemiloluwa@yahoo.com>
2020-10-03 19:13:27 +0100

    Initial Commit

---
+++ Shared/BuildNetflixAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixAppApp.swift
+//  Shared
+//
+//  Created by Temiloluwa on 03/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
